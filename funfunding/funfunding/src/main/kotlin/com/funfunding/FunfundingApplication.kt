430ade3e8ec6176816ec0c01bc2de61a273527ad
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2023-02-04 18:11:15 +0900

    hello

---
+++ funfunding/funfunding/src/main/kotlin/com/funfunding/FunfundingApplication.kt
@@ -0,0 +1,11 @@
+package com.funfunding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunfundingApplication
+
+fun main(args: Array<String>) {
+	runApplication<FunfundingApplication>(*args)
+}
